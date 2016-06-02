6bbc26d3679e3150a0371604d322637bf66de504
Colin W Kingen and Wolfgang Warneke <wolfgang@>
2016-06-02 10:04:38 -0700

    add array to img key values on objects to add alt text; add branching statements for animal selection; implement img src and alt attribute injection to branch statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 ///ANIMAL FARM
 var SNAKES = {
-  imgpath: "img/snake.jpg",
+  img: ["img/snake.jpg", "Picture of a snake"],
   name: "The Snake",
   blurb: "Our slithery friends",
   fact: "Indiana Jones is afraid of snakes.",
   info: "This is a paragraph about snakes. We'll add more later, okay?"
 };
 var TURTLES = {
-  imgpath: "img/turtle.jpg",
+  img: ["img/turtle.jpg", "Picture of a turtle"],
   name: "Turtle",
   blurb: "I like turtles",
   fact: "Jake the Zombie likes turtles too.",
   info: "This is a paragraph about turtles. We'll add more later, okay?"
 };
 var OCTOPI = {
-  imgpath: "img/octopus.jpg",
+  img: ["img/octopus.jpg", "Picture of an octopus"],
   name: "Octopus",
   blurb: "Not to be confused with squid",
   fact: "Indiana Jones is afraid of octopi as well.",
@@ -27,5 +27,16 @@ $(document).ready(function() {
   $(".animal h1").click(function() {
     $(".animalmenu, #infobox").toggleClass('hidden');
     var animalChoice = $(this).text();
+      if (animalChoice === "SNAKES") {
+        $("img").attr("src", SNAKES.img[0]);
+        $("img").attr("alt", SNAKES.img[1]);
+      } else if (animalChoice === "TURTLES") {
+        $("img").attr("src", TURTLES.img[0]);
+        $("img").attr("alt", TURTLES.img[1]);
+      } else {
+        $("img").attr("src", OCTOPI.img[0]);
+        $("img").attr("alt", OCTOPI.img[1]);
+      }
+
   });
 });
